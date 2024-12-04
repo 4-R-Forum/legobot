3964e53cfcc4dd8a6df764c526d2c6f87189dec7
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2024-12-04 17:54:47 -0500

    L-15

--- main.py
+++ main.py
@@ -1,138 +1,129 @@
-debug = 0
-heading_dev = 0
-heading_T = 0
-heading_thold = 0
-go = 0
+def on_button_pressed_ab():
+    global n
+    input.calibrate_compass()
+    n = 1
+    basic.show_icon(IconNames.SQUARE)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def calc_h_dev2(cur: number, tgt: number):
+    pass
+def stopMotors():
+    MotorDriver.motor_stop(Motor.A)
+    MotorDriver.motor_stop(Motor.B)
+def goFwd(speed: number, dist: number):
+    pass
+def calc_h_dev(cur2: number, tgt2: number):
+    global diff, heading_dev
+    diff = tgt2 - cur2
+    if diff > 0:
+        if diff > 180:
+            heading_dev = 180 - diff
+        else:
+            heading_dev = diff
+    else:
+        if diff < -180:
+            heading_dev = 360 + diff
+        else:
+            heading_dev = diff
+def TurnHeading(target: number):
+    if True:
+        # Dev +ve
+        # Turn Right, ClockWise
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            """)
+        # turn right
+        MotorDriver.motor_run(Motor.A, Dir.FORWARD, turnSpeed)
+        MotorDriver.motor_run(Motor.B, Dir.BACKWARD, turnSpeed)
+        basic.pause(10 * abs(heading_dev))
+        stopMotors()
+    if heading_dev < 0 - heading_thold:
+        # Dev -ve
+        # Turn Left, AntiClockwise
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            """)
+        # turn left
+        MotorDriver.motor_run(Motor.A, Dir.BACKWARD, turnSpeed)
+        MotorDriver.motor_run(Motor.B, Dir.FORWARD, turnSpeed)
+        basic.pause(10 * abs(heading_dev))
+        stopMotors()
+    # within range
+    if heading_dev >= 0 - heading_thold and heading_dev <= heading_thold:
+        if heading_dev >= 0 - 2 and heading_dev <= 2:
+            # Spot on, within 2 deg
+            basic.show_leds("""
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                """)
+        else:
+            # Within threshold
+            basic.show_leds("""
+                . . . . .
+                . . . . .
+                . # # # .
+                . . . . .
+                . . . . .
+                """)
+    stopMotors()
 
 def on_button_pressed_a():
-    global debug
-    debug = 0
+    pass
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def calc_h_dev(cur: number, tgt: number):
-    global heading_dev
-    if cur - tgt > 180:
-        heading_dev = cur - tgt - 360
-    else:
-        heading_dev = cur - tgt
-def TurnHeading(target: number):
-    while True:
-        calc_h_dev(input.compass_heading(), heading_T)
-        if heading_dev < 0 - heading_thold:
-            basic.show_leds("""
-                . . . . .
-                . . . . .
-                . . . . #
-                . . . . .
-                . . . . .
-                """)
-            # turn right
-            MotorDriver.motor_run(Motor.A, Dir.FORWARD, 8)
-            MotorDriver.motor_run(Motor.B, Dir.BACKWARD, 8)
-        if heading_dev > heading_thold:
-            basic.show_leds("""
-                . . . . .
-                . . . . .
-                # . . . .
-                . . . . .
-                . . . . .
-                """)
-            # turn left
-            MotorDriver.motor_run(Motor.A, Dir.BACKWARD, 8)
-            MotorDriver.motor_run(Motor.B, Dir.FORWARD, 8)
-        # within range
-        if heading_dev >= 0 - heading_thold and heading_dev <= heading_thold:
-            if heading_dev >= 0 - 2 and heading_dev <= 2:
-                basic.show_leds("""
-                    . . . . .
-                    . . . . .
-                    . . # . .
-                    . . . . .
-                    . . . . .
-                    """)
-            else:
-                basic.show_leds("""
-                    . . . . .
-                    . . . . .
-                    . # # # .
-                    . . . . .
-                    . . . . .
-                    """)
-            MotorDriver.motor_stop(Motor.A)
-            MotorDriver.motor_stop(Motor.B)
-            break
-def TurnHeadingtest():
-    global go, heading_dev
-    while heading_dev != 0:
-        while heading_dev < 0 - heading_thold or heading_dev > heading_thold:
-            if heading_dev < 0 - heading_thold:
-                # turn right
-                MotorDriver.motor_run(Motor.A, Dir.FORWARD, 8)
-                MotorDriver.motor_run(Motor.B, Dir.BACKWARD, 8)
-                go = 1
-                heading_dev += 1
-            if heading_dev > heading_thold:
-                # turn left
-                MotorDriver.motor_run(Motor.A, Dir.BACKWARD, 8)
-                MotorDriver.motor_run(Motor.B, Dir.FORWARD, 8)
-                go = -1
-                heading_dev += -1
-        MotorDriver.motor_stop(Motor.A)
-        MotorDriver.motor_stop(Motor.B)
-        go = 0
-        break
 def Forth():
-    MotorDriver.motor_run(Motor.A, Dir.FORWARD, 10)
-    MotorDriver.motor_run(Motor.B, Dir.FORWARD, 10)
+    MotorDriver.motor_run(Motor.A, Dir.BACKWARD, 10)
+    MotorDriver.motor_run(Motor.B, Dir.BACKWARD, 10)
     basic.pause(2000)
     MotorDriver.motor_stop(Motor.A)
     MotorDriver.motor_stop(Motor.B)
 
-def on_button_pressed_ab():
-    basic.show_icon(IconNames.SMALL_DIAMOND)
-    input.calibrate_compass()
-    basic.show_icon(IconNames.SQUARE)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
 def on_button_pressed_b():
-    global debug
-    debug = 1
+    global n
+    n += -1
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_logo_pressed():
-    global heading_T, heading_thold
-    basic.show_string("L13")
-    basic.pause(1000)
-    basic.clear_screen()
-    datalogger.delete_log(datalogger.DeleteType.FULL)
+    global turnSpeed, heading_T, heading_thold, compReading
+    basic.show_string("L15")
+    datalogger.delete_log(datalogger.DeleteType.FAST)
+    turnSpeed = 4
     heading_T = 0
-    heading_thold = 10
-    if debug == 1:
-        basic.show_number(input.compass_heading())
-        basic.show_number(heading_T)
-        calc_h_dev(input.compass_heading(), heading_T)
-        basic.show_number(heading_dev)
-    TurnHeading(0)
-    for index in range(4):
-        if heading_T == 0:
-            basic.show_string("N")
-        if heading_T == 90:
-            basic.show_string("E")
-        if heading_T == 180:
-            basic.show_string("S")
-        if heading_T == 270:
-            basic.show_string("W")
-        Forth()
-        heading_T += 90
-        if heading_T == 360:
-            heading_T = 0
+    heading_thold = 5
+    while True:
+        compReading = input.compass_heading()
+        calc_h_dev(compReading, heading_T)
+        datalogger.log(datalogger.create_cv("compass", compReading),
+            datalogger.create_cv("target", heading_T),
+            datalogger.create_cv("dev", heading_dev),
+            datalogger.create_cv("interval", n))
         TurnHeading(heading_T)
+        if heading_dev > heading_thold and heading_dev < 0 - heading_thold:
+            break
+        basic.pause(1000 * n)
     basic.show_icon(IconNames.YES)
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
+compReading = 0
+heading_T = 0
+heading_thold = 0
+turnSpeed = 0
+heading_dev = 0
+diff = 0
+n = 0
+basic.show_icon(IconNames.SMALL_DIAMOND)
+
 def on_forever():
-    calc_h_dev(input.compass_heading(), heading_T)
-    datalogger.log(datalogger.create_cv("compass", input.compass_heading()),
-        datalogger.create_cv("target", heading_T),
-        datalogger.create_cv("dev", heading_dev))
+    pass
 basic.forever(on_forever)
