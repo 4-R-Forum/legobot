5c8890c7c70407e5e7c297c8e6f977c0ff2f6a13
Jon Hodge <ghodge2016@outlook.com>
2025-02-05 15:19:15 -0500

    PDCA-LegoBotAI-01.2, test_turn successsful

--- PDCA/PDCA-LegoBotAI-01.2.md
+++ PDCA/PDCA-LegoBotAI-01.2.md
@@ -11,8 +11,13 @@ Incorporate POC for field test 1.2
 - Work/PCode-1.2.py tested in Micropython editor
     - function for log_yaw()
     - function for test_turn()
-    
+- Gyro branch cleaned up with new code in main.py
+
 ## CHECK
+- Successful test, see https://d.docs.live.net/c9fe7ec56ddc73db/_Gus2B/1-Projects/LegoBot/Gyro-PDCA-1.2.2.xlsx
 
 ## ACT
+- remove log_yaw() function
+- field test with motors next
+- results look so good ML may not be necessary!
 
--- main.py
+++ main.py
@@ -3,15 +3,22 @@ from microbit import *
 import struct, log, time
 
 MPU6050_ADDR = 0x68
-
-# Initialize MPU-6050
 def init_mpu6050():
+    # Function to initialize MPU-6050
+    global MPU6050_ADDR
     i2c.write(MPU6050_ADDR, bytearray([0x6B, 0]))  # Wake up MPU-6050
+def read_mpu6050(register):
+        # function to  Read 16-bit raw data from 6050 register
+        i2c.write(MPU6050_ADDR, bytearray([register]))  
+        data = i2c.read(MPU6050_ADDR, 2)  
+        return struct.unpack(">h", data)[0] 
+def get_yaw_velocity():
+    #Function to get yaw velocity (°/s)
+    return read_mpu6050(0x47) / 131.0  # Convert raw data to °/s 
 
 # Global Setup
 init_mpu6050()
-log.set_labels('yaw')
-
+log.set_labels('ang_vel','acc_yaw')
 
 # Parameters for single test
 HeadingChange = 90 # degrees
@@ -19,40 +26,32 @@ StopReadDelay = 100 # ms
 MotorPower = 4 # 40%
 # from L15 test turn with motor power 4 is 4 deg per second = 250 ms per degree
 # so expected duratiopn in this test = 90 * 250 = 22500 ms
-ExpectedDuration = 22500
+# ExpectedDuration = 22500
 sample_rate = 100 # 0.1 sec
 
-
-def log_yaw(sample_rate):
+def log_yaw():
+    ### created for POC in PDCA-01.1
+    ### unit test candidate ///TODO params for values to log
     # Read 16-bit raw data from a register
     def read_mpu6050(register):
         i2c.write(MPU6050_ADDR, bytearray([register]))  
         data = i2c.read(MPU6050_ADDR, 2)  
         return struct.unpack(">h", data)[0]  
-
     # Get yaw velocity (°/s)
     def get_yaw_velocity():
         return read_mpu6050(0x47) / 131.0  # Convert raw data to °/s
-
-
     # Variables for integration
     yaw_angle = 0.0  # Initial heading
     start_time = time.ticks_ms()   # Get initial time in milliseconds
     last_time = start_time
-
     while (time.ticks_diff(time.ticks_ms(), start_time) < ExpectedDuration) :
         # Read yaw rate
-        yaw_rate = get_yaw_velocity()
-        
-        # Get current time and compute time difference (dt in seconds)
-        
+        yaw_rate = get_yaw_velocity()      
         # Integrate yaw rate to update heading
         this_time = time.ticks_ms()
         yaw_angle += yaw_rate * (this_time  - last_time) / 1000
-
         # Normalize to ±180° format
         yaw_angle = (yaw_angle + 180) % 360 - 180  
-
         # Log heading change
         print("Yaw (°):", yaw_angle)
         log.add({
@@ -60,25 +59,34 @@ def log_yaw(sample_rate):
         })
         sleep(sample_rate)  # Adjust sample rate
 
-def test_turn ():
-    global turnSpeed, heading_T, heading_thold, compReading
-    basic.show_string("L15")
-    datalogger.delete_log(datalogger.DeleteType.FAST)
-    turnSpeed = 4
-    heading_T = 0
-    heading_thold = 5
-    while True:
-        compReading = input.compass_heading()
-        calc_h_dev(compReading, heading_T)
-        datalogger.log(datalogger.create_cv("compass", compReading),
-            datalogger.create_cv("target", heading_T),
-            datalogger.create_cv("dev", heading_dev),
-            datalogger.create_cv("interval", n))
-        TurnHeading(heading_T)
-        if heading_dev > heading_thold and heading_dev < 0 - heading_thold:
-            break
-        basic.pause(1000 * n)
-    basic.show_icon(IconNames.YES)    
+def test_turn():
+    log.delete()
+    ### copied from log_yaw in PDCA-01.1
+    ### unit test candidate ///TODO params for values to log
 
-
-log_yaw(100)
+    # Variables for integration
+    yaw_angle = 0.0  # Initial heading
+    start_time = time.ticks_ms()   # Get initial time in milliseconds
+    last_time = start_time
+    while (time.ticks_diff(time.ticks_ms(), start_time) < 2000) : # Simultor test
+    # while (time.ticks_diff(time.ticks_ms(), start_time) < 2000) : # Field test
+        # Read yaw rate
+        ang_vel = get_yaw_velocity()      
+        # Integrate yaw rate to update heading
+        this_time = time.ticks_ms()
+        yaw_angle += ang_vel * (this_time  - last_time) / 1000
+        # Normalize to ±180° format
+        yaw_angle = (yaw_angle + 180) % 360 - 180  
+        # Log heading change
+        print("AngVel (°/s)", ang_vel, ", Yaw (°):",yaw_angle)
+        log.add({
+        'ang_vel': ang_vel,
+        'acc_yaw': yaw_angle
+        })
+        sleep(sample_rate)  # Adjust sample rate
+        
+# main code #
+display.show("G1.2")
+display.clear()
+test_turn()
+display.show(Image.YES)
