e749e5bc397e7745d30463898a14820e8e5aad5b
Jon Hodge <ghodge2016@outlook.com>
2025-02-13 11:44:25 -0500

    1.5.1.5 main.py corrected wrong paste

--- main.py
+++ main.py
@@ -1,44 +1,117 @@
 from microbit import *
+import log, time, gc
+import sbcmotorcontroller
+pdca = "G1.5.1"
 
-## SBC Motor Controller
-# Define motor and servo pins
-PWMA   = pin8  # Analog Motor A
-AIN1   = pin13 # Digital
-AIN2   = pin12 # Digital
-PWMB   = pin16 # Analog Motor B
-BIN1   = pin14 # Digital
-BIN2   = pin15 # Digital
-S0_PIN = pin0  # Analog Servo1
-S1_PIN = pin1  # Analog Servo2
-S2_PIN = pin2  # Analog Servo3
-
-def motor_run(motor: int, direction: int, speed: int):
-    duty = min(max(speed * 64 - 1, 0), 1023)  # Map 0-16 to 0-1023
-    global AIN1, AIN2, BIN1, BIN2, PWMA, PWMB
-    if motor == 1:
-        #Pins.analogWritePin(PWMA, duty)
-        PWMA.write_analog(duty)
-        PWMA.set_analog_period(1)
-        if direction == 1:
-            AIN1.write_digital(0)
-            AIN2.write_digital(1)
-        else:
-            AIN1.write_digital(1)
-            AIN2.write_digital(0)
+MPU6050_ADDR = 0x68
+def init_mpu6050():
+    # Function to initialize MPU-6050
+    global MPU6050_ADDR
+    i2c.write(MPU6050_ADDR, bytearray([0x6B, 0]))  # Wake up MPU-6050
+def read_mpu6050(register):
+        # function to  Read 16-bit raw data from 6050 register
+        i2c.write(MPU6050_ADDR, bytearray([register]))  
+        raw = i2c.read(MPU6050_ADDR, 2)   # read 2 bytes
+        value = (raw[0] << 8) | raw[1]  # Combine High Byte and Low Byte
+        # Convert to signed 16-bit integer
+        if value > 32767:
+            value -= 65536 
+        return value
+def get_yaw_velocity():
+    #Function to get yaw velocity (°/s)
+    return read_mpu6050(0x47) / 131.0  # Convert raw data to °/s 
+def dev_update(change, yaw):
+    if change > 0:
+        return change -yaw
     else:
-        PWMB.write_analog(duty)
-        PWMB.set_analog_period(1)
-        if direction == 1:
-            BIN1.write_digital(0)
-            BIN2.write_digital(1)
+        return change + yaw
+def turn_complete(change, dev):
+    if change > 0:
+        if dev < 0:
+            return True
         else:
-            BIN1.write_digital(1)
-            BIN2.write_digital(0)
+            return False
+    elif change < 0:
+        if dev > 0:
+            return True
+        else:
+            return False
 
-def motor_stop(motor:int):
-    if motor == 1:
-        PWMA.write_analog(0)
+# Global Setup
+init_mpu6050()
+log.set_labels('ang_vel','acc_yaw','dev')
+# Issues sharing enums/Class between modules, use int
+MotorA = 1
+MotorB = 2
+DirFWD = 1
+DirBCK = 2
+
+def turn_heading_test(HeadingChange, MotorPower, sample_rate):
+    HeadingDev = 0 - HeadingChange
+    yaw_angle = 0
+    start_time = time.ticks_ms()   # Get initial time in milliseconds
+    last_time = start_time
+    log.add({
+    'ang_vel': 0,
+    'acc_yaw': 0,
+    'dev': HeadingChange
+    })
+    while True:
+        # start fwd motor first, avoid turn wrong way
+        if HeadingDev < 0:
+            # turn left, CCW, angvel +ve
+            Adir = DirFWD
+            Bdir = DirBCK
+            sbcmotorcontroller.motor_run(MotorA, Adir, MotorPower)
+            sbcmotorcontroller.motor_run(MotorB, Bdir, MotorPower)
+        else:
+            # turn right, CW, angvel -ve
+            Adir = DirBCK
+            Bdir = DirFWD
+            sbcmotorcontroller.motor_run(MotorB, Bdir, MotorPower)
+            sbcmotorcontroller.motor_run(MotorA, Adir, MotorPower)
+       
+        # Read yaw rate
+        ang_vel = get_yaw_velocity()      
+        # Integrate yaw rate to update heading
+        this_time = time.ticks_ms()
+        yaw_angle += ang_vel * (this_time  - last_time) / 1000 # dt +ve
+        # Normalize to ±180° format
+        yaw_angle = (yaw_angle + 180) % 360 - 180
+        HeadingDev = dev_update(HeadingChange, yaw_angle) # ///TODO Test
+        # Log heading change
+        # print("AngVel (°/s)", ang_vel, ", Yaw (°):",yaw_angle) # for simulator test
+        log.add({
+        'ang_vel': ang_vel,
+        'acc_yaw': yaw_angle,
+        'dev': HeadingDev
+        })
+        if turn_complete(HeadingChange,HeadingDev): # /// TODO Test
+            log.add({
+            'ang_vel': ang_vel,
+            'acc_yaw': yaw_angle,
+            'dev': HeadingDev
+            })
+            break
+        sleep(sample_rate)  # Adjust sample rate
+
+    if HeadingDev < 0: 
+        # avoid turning wrong way on stop
+        sbcmotorcontroller.motor_stop(MotorA)
+        sbcmotorcontroller.motor_stop(MotorB)
     else:
-        PWMB.write_analog(0)
-
-## end of SBC MotorController
+        sbcmotorcontroller.motor_stop(MotorB)
+        sbcmotorcontroller.motor_stop(MotorA)
+ 
+# main code #
+display.show(pdca)
+display.clear()
+log.delete()
+turn_heading_test(30, 3, 80)
+sleep(2000)
+turn_heading_test(60, 3, 80)
+sleep(2000)
+turn_heading_test(-30, 3, 80)
+sleep(2000)
+turn_heading_test(-60, 3, 80)
+display.show(Image.YES)
